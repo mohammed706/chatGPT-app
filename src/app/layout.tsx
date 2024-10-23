20785d1a2a667c6cd0eb3d05b51d0f2f6a4919fc
Mohammed Sheikh Khamis <mohammed.khamis@check24.de>
2024-10-23 19:42:01 +0200

    Adding Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +29,18 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <header className="text-white font-bold bg-green-900 text-2xl p-2 mb-3 rounded-b-lg shadow-gray-700 shadow-lg flex">
+          <div className="flex flex-grow">
+            <Link href="/">GPT Chat</Link>
+            <Link href="/About" className="ml-5 font-light">
+              About
+            </Link>
+          </div>
+          <div></div>
+        </header>
+        <div className="flex flex-col md:flex-row">
+          <div className="flex-grow">{children}</div>
+        </div>
       </body>
     </html>
   );
